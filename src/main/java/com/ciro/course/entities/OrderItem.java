f5b0e499bd9604f11dcf971239a7e6474a32e6bc
Ciro Junqueira <cirojunqueira1@gmail.com>
2020-11-28 17:26:49 -0300

    Subtotal & Total methods

--- src/main/java/com/ciro/course/entities/OrderItem.java
+++ src/main/java/com/ciro/course/entities/OrderItem.java
@@ -64,6 +64,9 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public double getSubTotal() {
+		return price*quantity;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
